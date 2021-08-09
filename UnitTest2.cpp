8582ebecd4f293112682026d3e1aac9b030ac9f6
romikvers <koshivr032@gmail.com>
2021-08-09 10:33:22 +0300

    second test

---
+++ UnitTest2.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../befor_test/befor_test.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = sum(2, 3);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
